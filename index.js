0110e0955b85661694d387544478b4915ac031d2
lavanyeswari <lavanyeswari@gmail.com>
2020-09-03 09:26:18 +0530

    Var basics

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var x = 1;
+if (x === 1) {
+  var x = 2;
+  console.log(x);
+}
+console.log(x);
